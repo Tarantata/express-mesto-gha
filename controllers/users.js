8c7172404c28114f1ee824e4e4153df1de217731
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2022-12-10 19:47:19 +0200

    commit 10.12.2022

--- controllers/users.js
+++ controllers/users.js
@@ -79,7 +79,7 @@ const updateUserAvatar = async (req, res) => {
     if (user === null) {
       return res.status(404).json({ message: 'Пользователь с указанным _id не найден' });
     }
-    return res.status(201).json(user);
+    return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json({ message: 'Произошла ошибка' });
   }
