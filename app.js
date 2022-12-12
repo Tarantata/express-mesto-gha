746852fd7ad80af7ed2a5e4230e9f66c9f5c6d17
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2022-12-12 19:43:03 +0200

    commit 12.12.2022

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-import rateLimit from 'express-rate-limit';
-
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-// const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
 const routerUser = require('./routes/users');
 const routerCard = require('./routes/cards');
 
 const PORT = 3000;
 const app = express();
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-// Apply the rate limiting middleware to all requests
-app.use(limiter);
 
 app.use(helmet());
 
