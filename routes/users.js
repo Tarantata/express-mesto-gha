e4d582a4139d897769b8bad360e8ddd0a6e57502
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2022-12-26 12:49:05 +0200

    commit 25.12.2022

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,17 @@ const routerUser = require('express').Router();
 const {
   getUsers, getUserById, updateUserProfile, updateUserAvatar, getUserInfo, createUser,
 } = require('../controllers/users');
+const {validateGetUserById, validateUpdateProfile, validateUpdateAvatar} = require("../middlewares/validation");
 
 routerUser.get('/', getUsers);
 
 routerUser.post('/', createUser);
 
-routerUser.get('/:_id', getUserById);
+routerUser.get('/users/:_id', validateGetUserById, getUserById);
 
-routerUser.patch('/me', updateUserProfile);
+routerUser.patch('/users/me', validateUpdateProfile, updateUserProfile);
 
-routerUser.patch('/me/avatar', updateUserAvatar);
+routerUser.patch('/users/me/avatar', validateUpdateAvatar, updateUserAvatar);
 
 routerUser.get('/users/me', getUserInfo);
 
